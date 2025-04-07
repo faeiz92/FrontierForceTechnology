f03d232886c2421d657a10304ddf45f6eed41d9b
faeiz.salleh <faeiz.salleh@microlink.com.my>
2025-04-07 14:36:53 +0800

    frontierforcetechnology

---
+++ FrontierForceTechnology/DTO/CountryDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace ProjectB.DTO
+{
+    public class CountriesResponse
+    {
+        public List<CountryDTO> Countries { get; set; }
+    }
+    public class CountryDTO
+    {
+        public string CountryName { get; set; }
+        public string CountryCode { get; set; }
+        public int FFCountrySID { get; set; }
+    }
+}
---
+++ FrontierForceTechnology/MappingProfile/MappingProfile .cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+
+namespace ProjectB.MappingProfile
+{
+    using AutoMapper;
+    using ProjectA.DTO;
+    using ProjectB.DTO;
+
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<OrderHeader, OrderHeaderDTO>()
+            .ForMember(dest => dest.OrderDetails, opt => opt.MapFrom(src => src.OrderDetails));
+
+            // Mapping for OrderDetail
+            CreateMap<OrderDetail, OrderDetailDTO>()
+                .ForMember(dest => dest.ConsignorCountryCode, opt => opt.Ignore()) 
+                .ForMember(dest => dest.ConsignorCountryName, opt => opt.Ignore());
+        }
+    }
+
+}
